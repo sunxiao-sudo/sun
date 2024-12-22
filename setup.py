e36f5c87fdb425b169d59190f50b6af5b369b83a
sunxiao-sudo <152365413+sunxiao-sudo@users.noreply.github.com>
2024-12-22 12:02:15 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,22 @@ package_name = 'goal_publisher'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/ament_package', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml', 'goal_publisher/goal_publisher.py']),
     ],
     install_requires=['setuptools', 'rclpy', 'geometry_msgs'],
     zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
-    description='A ROS 2 package to publish goals and engage scripts',
+    maintainer='Your Name',
+    maintainer_email='your.email@example.com',
+    description='Goal publisher node for mission planning',
     license='Your License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'goal_publisher = goal_publisher.goal_publisher:main',  # 添加这一行
+            'goal_publisher = goal_publisher.goal_publisher:main',
         ],
     },
 )
