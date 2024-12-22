ae115f65ad5e23a5b68f5640b482c3ab78e10aab
sunxiao-sudo <152365413+sunxiao-sudo@users.noreply.github.com>
2024-12-22 11:08:51 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,12 @@ package_name = 'goal_publisher'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='your_email@example.com',
-    description='Goal publisher for ROS2',
-    license='License',
-    tests_require=['pytest'],
+    install_requires=['setuptools', 'rclpy', 'geometry_msgs'],
     entry_points={
         'console_scripts': [
-            'goal_publisher = goal_publisher.goal_publisher:main',  # 确保 main() 方法的路径正确
+            'goal_publisher = goal_publisher.goal_publisher:main',  # 注意这里的配置
         ],
     },
 )
